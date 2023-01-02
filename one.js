6f540c12e1fbf6ef4dcedf3aa64d540e0229c406
Mikhail Sprygin <wave103x@gmail.com>
2023-01-02 16:27:26 +0300

    feat: add some tasks

--- one.js
+++
@@ -1,6 +0,0 @@
-function swapHeadAndTail(arr) {
-  return arr.slice((Math.round(arr.length / 2)))
-    .concat(arr.slice(Math.floor(arr.length / 2), Math.ceil(arr.length / 2)))
-    .concat(arr.slice(0, Math.floor(arr.length / 2)));
-}
-console.log(swapHeadAndTail([1, 2]));
--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -271,10 +271,9 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.map((e, i) => Array(i + 1).fill(e)).flat();
 }
-
 
 /**
  * Returns the 3 largest numbers from the specified array
@@ -289,8 +288,8 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.sort((a, b) => b - a).slice(0, 3);
 }
 
 
